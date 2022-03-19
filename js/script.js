17e1ae9f5b5a318571d944bead67e70a90e9738a
Joy Christine <joychristin2@gmail.com>
2022-03-19 21:06:53 +0300

    add event listener on form btn

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
+//store prices, crust ,size, toppings in array
+//create loop such that 
+let pizzaPrice = {
+    small:600,
+    medium:800,
+    large:1000
+}
+let toppingPrice = [{
+    Tomato:80,
+    Onions:80,
+    Mushroom:80,
+    Olives:120,
+    Pineapple:120,
+    Bacon:120,
+    Mince:120,
+    Beef:170,
+    Chicken:170
+
+
+}];
+
+function pizzaSize(price) {
+    if (size === 'small'){
+         return (price.small* 1);
+    }else if (size === 'medium'){
+        return (price.medium * 1);
+    }else {
+        return(price.large * 1);
+    }
+}
+
+// make-btn get all values filled in the form
+$(document).ready(function() {
+    $("#make-order").click(function() {
+        let size = $('#size option:selected').val();
+        let flavour = $('#flavour option:selected').val();
+        let crust = $('#crust option:selected').val();
+        let toppings = $('#toppings option:selected').val();
+        let number = $("#pizzaNumber").val();
+        // console.log(flavour); 
+    });
+});
+
+
+
+
+
+
+
+
 
 // pick up btn alert 
 $(document).ready(function() {
