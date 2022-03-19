f1f6cdbce4c2c9c4b2ba7506d99402b18aa593a0
Joy Christine <joychristin2@gmail.com>
2022-03-19 13:10:24 +0300

    add alert on click the pick-up btn

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
+
+// pick up btn alert 
 $(document).ready(function() {
-    $("#submit").click(function(event){
-        var person = document.getElementById("fname").value;
-        var street = document.getElementById("street").value;
-        var building = document.getElementById("building").value;
-        var house  = document.getElementById("hseNo").value;
-        alert('Dear' + " " + person + " "+ 'we have received your order!. It will be delivered at' + " "+
-         street + " "+ 'street,' + " " + 'building'+ " " + building + " "  + " "+ 'house number' + " " + house + " " + " "+
-         'Thank you for reaching out to us.');
+    $(".pick").click(function(event){
+        alert('we are waiting for you!');
         event.preventDefault();
     });
 });
