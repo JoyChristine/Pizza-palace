38356cb73d5bd05788f2ef7c96e54056f1f4b81b
Joy Christine <joychristin2@gmail.com>
2022-03-19 14:18:00 +0300

    hide delivery form & add jquery methods to buttons

--- js/script.js
+++ js/script.js
@@ -2,9 +2,25 @@
 // pick up btn alert 
 $(document).ready(function() {
     $(".pick").click(function(event){
-        alert('we are waiting for you!');
+        alert('Thanks for your order! We are waiting for you!');
         event.preventDefault();
     });
+    //hide delivery form
+    $("#delivery").hide();
+    //make delivery form appear onclick the make delivery btn
+    $(".make").click(function() {
+        $('#delivery').toggle();
+    })
+    //alert customer that details have been recorded
+    $('#submit-delivery').click(function(event) {
+        var person = document.getElementById("fname").value;
+        var street = document.getElementById("street").value;
+        var house = document.getElementById("hseNo").value;
+
+        alert('Dear' + " " + person + " "+ 'we have received your message.' + 'We will deliver in 15 minutes to House Number ' + "" + house + "," + 'on' + ' ' + street + " street." + 'Thank you for ordering with us.');
+        event.preventDefault();
+
+    })
 });
 
 
